6efe70c23aed7a27aad69b4e0df703776a2abca5
zand <zabahey.sukoi@gmail.com>
2018-03-07 20:30:23 +0700

    test change

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = new express();
 
 app.get('/', (req, res) => {
-    res.send({ bye: "buddy"});
+    res.send({ bye: "buddy22"});
 });
 
 const PORT = process.env.PORT || 5000;
