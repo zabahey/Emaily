085ba67b1df63df904f930ebe2416fbb3519d797
Parinya Samrongsap <zand.p@excelbangkok.com>
2018-03-12 14:59:36 +0700

    done oauth google authentication process

--- server/services/passport.js
+++ server/services/passport.js
@@ -12,14 +12,16 @@ passport.use(
         callbackURL: '/auth/google/callback'
     },
         (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id})
+            User.findOne({ googleId: profile.id })
                 .then((existingUser) => {
                     if (existingUser) {
-
+                        done(null, existingUser);
                     }
                     else {
-                        new User({ googleId: profile.id }).save();
+                        new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
                     }
                 })
-        })
+        }) 
 );
