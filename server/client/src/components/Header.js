877e3f16e260440d957a8a9a88f4b8eaed38310f
zabahey <zabahey.sukoi@gmail.com>
2018-03-20 14:45:19 +0700

    design header component with materialize css

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>Header</div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="#" className="left brand-logo">Emaily</a>
+                    <ul className="right">
+                        <li><a href="#">Login With Google</a></li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
