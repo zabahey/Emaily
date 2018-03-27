1e9a3592f73e91f25600c0d8d07d06e65b6b4c2c
zand <zabahey.sukoi@gmail.com>
2018-03-28 00:12:47 +0700

    user link component to redirect when click on logo

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
-        switch(this.props.auth){
+        switch (this.props.auth) {
             case null:
                 return;
             case false:
@@ -18,7 +19,12 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">Emaily</a>
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
+                        className="left brand-logo"
+                    >
+                        Emaily
+                    </Link>
                     <ul className="right">
                         {this.renderContent()}
                     </ul>
