d29e3c4d884195422d3a14f96d01a002a11c3fc7
zand <zabahey.sukoi@gmail.com>
2018-03-27 23:20:31 +0700

    display content depend on auth

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -5,11 +5,13 @@ class Header extends Component {
     renderContent() {
         switch(this.props.auth){
             case null:
-                return 'Still deciding';
+                return;
             case false:
-                return 'im loggedout';
+                return (
+                    <li><a href="/auth/google">Login With Google</a></li>
+                );
             default:
-                return 'im logged in'
+                return <li><a href="">Logout</a></li>
         }
     }
     render() {
